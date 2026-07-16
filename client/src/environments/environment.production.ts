7bfdc8b2ec97886f1dc0450ab50df8f2b985ada6
Gerardo Meiro Mendoza <gerardo.meiro@gmail.com>
2026-07-16 05:42:26 +0200

    Actualizacion direccion server

--- client/src/environments/environment.production.ts
+++ client/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     favicon: 'favicon-prod.ico', // O la ruta donde lo hayas guardado
-    apiUri: 'https://friendcup-production.up.railway.app/api' // 📌 Ojo: quité el :8080 porque Railway mapea al puerto 443 estándar de HTTPS automáticamente
+    apiUri: 'https://friendcup.onrender.com/api' // 📌 Ojo: quité el :8080 porque Railway mapea al puerto 443 estándar de HTTPS automáticamente
 };
